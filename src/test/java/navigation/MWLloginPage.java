b6a2ef23528b2aaa927d81c5b5156d14c0f77a64
MPTestDan <Daniel.Morgan@moneypenny.co.uk>
2021-05-24 17:48:54 +0100

    Initial import

---
+++ src/test/java/navigation/MWLloginPage.java
@@ -0,0 +1,7 @@
+package navigation;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("page:test.page")
+class MWLloginPage extends PageObject {}
---
+++ src/test/java/starter/CucumberTestSuite.java
@@ -0,0 +1,14 @@
+package starter;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+//        features="src/test/resources/features/search/search_by_keyword.feature"
+        features="src/test/resources/features/search/bookingTimeOff.feature"
+//        features = "classpath:features"
+)
+public class CucumberTestSuite extends BrowserStackSerenityTest {}
